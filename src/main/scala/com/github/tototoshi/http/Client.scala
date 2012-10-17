8f28eca948849ccd9f34a6109c20181f0f09eff7
toshiyuki <t.toshi.0412@gmail.com>
2012-10-17 21:11:21 +0900

    Refactoring Update

--- src/main/scala/com/github/tototoshi/http/Client.scala
+++ src/main/scala/com/github/tototoshi/http/Client.scala
@@ -13,6 +13,7 @@ import org.apache.http.{ HttpResponse, NameValuePair, HttpEntity }
 import org.apache.http.util.EntityUtils
 import net.liftweb.json.JsonParser._
 import net.liftweb.json.JValue
+
 trait Using {
   type Closable = { def close(): Unit }
   def using[A <: Closable, B](resource: A)(f: A => B) = {
@@ -25,7 +26,7 @@ trait Using {
 }
 
 class Client extends Using {
-  def httpClient = {
+  private def httpClient = {
     val c = new DefaultHttpClient()
     val routePlanner = new ProxySelectorRoutePlanner(
       c.getConnectionManager.getSchemeRegistry, ProxySelector.getDefault)
@@ -53,30 +54,30 @@ class Client extends Using {
     new Response(httpClient.execute(request))
   }
 
-  class Response(httpResponse: HttpResponse) {
-    def statusCode(): Int = {
-      EntityUtils.consume(httpResponse.getEntity)
-      httpResponse.getStatusLine.getStatusCode
-    }
-    def asString(): String = asString("UTF-8")
-    def asString(charset: String): String = {
-      val res = EntityUtils.toString(httpResponse.getEntity, charset)
-      EntityUtils.consume(httpResponse.getEntity)
-      res
-    }
-    def asJson(): JValue = parse(asString)
-    def save(filename: String): Unit = save(new File(filename))
-    def save(file: File): Unit = {
-      using(new java.io.BufferedInputStream(httpResponse.getEntity.getContent)) { in =>
-        using(new java.io.PrintStream(file)) { out =>
-          val buffer = new Array[Byte](8192)
-          Stream.continually(in.read(buffer)).takeWhile(_ >= 0) foreach {
-            out.write(buffer, 0, _)
-          }
+}
+
+class Response(httpResponse: HttpResponse) extends Using {
+  def statusCode(): Int = {
+    EntityUtils.consume(httpResponse.getEntity)
+    httpResponse.getStatusLine.getStatusCode
+  }
+  def asString(): String = asString("UTF-8")
+  def asString(charset: String): String = {
+    val res = EntityUtils.toString(httpResponse.getEntity, charset)
+    EntityUtils.consume(httpResponse.getEntity)
+    res
+  }
+  def asJson(): JValue = parse(asString)
+  def save(filename: String): Unit = save(new File(filename))
+  def save(file: File): Unit = {
+    using(new java.io.BufferedInputStream(httpResponse.getEntity.getContent)) { in =>
+      using(new java.io.PrintStream(file)) { out =>
+        val buffer = new Array[Byte](8192)
+        Stream.continually(in.read(buffer)).takeWhile(_ >= 0) foreach {
+          out.write(buffer, 0, _)
         }
       }
     }
   }
-
 }
 
